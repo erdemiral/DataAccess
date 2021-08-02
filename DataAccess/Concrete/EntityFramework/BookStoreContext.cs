0052febcdfac01768a986b17a7aded6bbc9ca366
Erdem <hikmeterdemiral@gmail.com>
2021-08-02 11:42:32 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/BookStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class BookStoreContext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Data Source=.\sqlexpress;Database=BookStore;integrated security=True");
+        }
+            
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
